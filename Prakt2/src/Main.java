b1f732a56dbf44e99094aee3877cfadcada6e68e
“Sashko“ <haosgg@ukr.net>
2024-03-04 21:08:40 +0200

    First

---
+++ Prakt2/src/Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //task 1
+        int a = 10;
+        int b = 5;
+        int c = a/b;
+        int d = a%b;
+        System.out.println("Результат їх ділення:" + c);
+        System.out.println("Результат залишку від ділення:" + d);
+//task 2
+        int f = 15;
+        int j = f/10;
+        int k = f%10;
+        int l = k+j;
+        System.out.println("Сума чисел двозначного числа: " + l);
+        //task 3
+        double Number = 34.4;
+        long numberR = (long) (Number +0.5);
+        System.out.println("Округлене число: " + numberR);
+
+
+        }
+    }
